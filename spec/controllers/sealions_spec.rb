551ffcbf4252d783979bbdbbad2a42ad7c08d740
Dan Asselin <dasselin86@gmail.com>
2015-03-17 22:16:31 -0400

    Create test for put sealions route

--- spec/controllers/sealions_spec.rb
+++ spec/controllers/sealions_spec.rb
@@ -8,11 +8,19 @@ describe 'Sealions Controller' do
     expect(last_response.body).to include('Welcome to the Sealions page')
   end
 
-  it 'Should process the \'post/sea_post\' route' do
+  it 'Should process the post/sea_post route' do
     post '/sea_post'
     expect(last_response).to be_redirect
     follow_redirect!
     expect(last_response.status).to eq(200)
+    expect(last_response.body).to include('Welcome to the Sealions page!')
+  end
+
+  it 'Should process the put/sea_update route' do
+    put '/sea_update'
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_response).to be_ok
     expect(last_response.body).to include('Welcome to the Sealions page!')
   end
 end
