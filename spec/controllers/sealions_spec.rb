c0acc18fcff2c5e242394739fdfe90c164851407
Dan Asselin <dasselin86@gmail.com>
2015-03-17 21:51:38 -0400

    Move Gemfile and Gemfile.lock to source to be recognized by all subdirectories

--- spec/controllers/sealions_spec.rb
+++ spec/controllers/sealions_spec.rb
@@ -1,2 +1,10 @@
-require_relative 'spec_helper'
+require_relative '../spec_helper'
 
+describe 'Sealions Controller' do
+
+  it 'Should get the /sealions route' do
+    get '/sealions'
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('Welcome to Pier 93')
+  end
+end
