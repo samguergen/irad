ae989eaffa6c8f1259a975df68dd60b745aa22ce
Dan Asselin & Malcolm Bouzi & Awesome Ninja Fighter <pair+danasselin.mbouzi.samguergen@devbootcamp.com>
2015-03-20 12:03:53 -0400

    Delete lightsaber files - migration rb and cleared seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,2 @@
 
-sealions = {lion_1: {name: "Harrylion", age:47, quirk: "Afraid of helicopters"},
-            lion_2: {name: "Brendalion", age: 38, quirk:"Wears sunglasses in the shower"},
-            lion_3: {name: "Charlottelion", age: 18, quirk: "Only dates sealions that have had run-ins with sharks"}}
-
-sealions.each do |key, value|
-  Sealion.create!(value)
-end
 
