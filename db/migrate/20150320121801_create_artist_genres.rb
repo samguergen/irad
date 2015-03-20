31f98138f39cbcf906358deb2427a5cbbebd9fa0
Dan Asselin & Malcolm Bouzi & Awesome Ninja Fighter <pair+danasselin.mbouzi.samguergen@devbootcamp.com>
2015-03-20 16:36:08 -0400

    artist_genres migration detail

--- db/migrate/20150320121801_create_artist_genres.rb
+++ db/migrate/20150320121801_create_artist_genres.rb
@@ -1,6 +1,8 @@
         class CreateArtistGenres < ActiveRecord::Migration
           def change
-            create_table :artist_genres |t|
-            t.belongs_to
+            create_table :artist_genres do |t|
+              t.references :artist, index: true
+              t.references :genre, index: true
+            end
           end
         end
