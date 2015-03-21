56dfd4c467dfc5859fa0218e2848b6a12f6aa56d
Dan Asselin & Malcolm Bouzi & Awesome Ninja Fighter <pair+danasselin.mbouzi.samguergen@devbootcamp.com>
2015-03-21 14:27:50 -0400

    Write post route for new artist. Have not tested yet

--- app/controllers/artists.rb
+++ app/controllers/artists.rb
@@ -5,15 +5,31 @@ get '/artists' do
 end
 
 post '/artists' do
-  label = params[:record_label].strip
-  new_artist = Artist.new(moniker: params[:moniker], birth_name:params[:birth_name], age: params[:age], description: params[:description], record_label: artist_label)
-  genre = params[:genre].strip.downcase
+
+  label = params[:record_label].strip if params[:record_label]
+  genre = params[:genre].strip.downcase if params[:genre]
+
+  new_artist = Artist.create(
+                          moniker: params[:moniker],
+                          birth_name:params[:birth_name],
+                          age: params[:age],
+                          description: params[:description]
+                          )
+
+  if RecordLabel.find_by(name: label)
+    valid_label = Label.find_by(name: label)
+    new_artist.record_label_id = valid_label.id
+  else
+    "that record label is not in the database"
+    # eventually this should reroute to a 'create new label' page
+  end
 
   if Genre.find_by(name: genre)
     valid_genre = Genre.find_by(name: genre)
     new_artist << valid_genre
   else
-    Genre.create(name: genre)
+    "that genre is not in the database"
+    # this should reroute to a 'create new genre' page
   end
 
 end
