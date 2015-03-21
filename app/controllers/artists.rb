d26e85b7d042ab95694bd0fc27e8e11f2a0229a5
Dan Asselin & Malcolm Bouzi & Awesome Ninja Fighter <pair+danasselin.mbouzi.samguergen@devbootcamp.com>
2015-03-21 14:14:08 -0400

    Make all genre names in downcase form

--- app/controllers/artists.rb
+++ app/controllers/artists.rb
@@ -3,3 +3,18 @@ require 'sinatra'
 get '/artists' do
   erb :'artists/index', locals: {all_artists: Artist.all}
 end
+
+post '/artists' do
+  label = params[:record_label].strip
+  new_artist = Artist.new(moniker: params[:moniker], birth_name:params[:birth_name], age: params[:age], description: params[:description], record_label: artist_label)
+  genre = params[:genre].strip.downcase
+
+  if Genre.find_by(name: genre)
+    valid_genre = Genre.find_by(name: genre)
+    new_artist << valid_genre
+  else
+    Genre.create(name: genre)
+  end
+
+end
+
