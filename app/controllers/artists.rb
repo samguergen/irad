fe1d230605a8ae832c40b256ca8ec3f21ca9adb9
Dan Asselin & Malcolm Bouzi & Awesome Ninja Fighter <pair+danasselin.mbouzi.samguergen@devbootcamp.com>
2015-03-21 14:55:17 -0400

    added working link to new artist form, finished read route

--- app/controllers/artists.rb
+++ app/controllers/artists.rb
@@ -1,26 +1,37 @@
 require 'sinatra'
 
 get '/artists' do
-  erb :'artists/index', locals: {all_artists: Artist.all}
+  @all_artists = Artist.all
+  erb :'artists/index'
+end
+
+get '/artists/new' do
+  erb :'artists/new'
+end
+
+get '/artists/:id' do
+ @the_artist = params[:id]
+ erb :'artists/show'
 end
 
 post '/artists' do
 
-  label = params[:record_label].strip if params[:record_label]
-  genre = params[:genre].strip.downcase if params[:genre]
+  # label = params[:record_label].strip if params[:record_label]
+  # genre = params[:genre].strip.downcase if params[:genre]
 
   new_artist = Artist.create(
                           moniker: params[:moniker],
                           birth_name:params[:birth_name],
                           age: params[:age],
-                          description: params[:description]
+                          description: params[:description],
+                          genre: params[:genre]
                           )
 
   if RecordLabel.find_by(name: label)
     valid_label = Label.find_by(name: label)
     new_artist.record_label_id = valid_label.id
   else
-    "that record label is not in the database"
+    [500,"that record label is not in the database"]
     # eventually this should reroute to a 'create new label' page
   end
 
@@ -28,7 +39,7 @@ post '/artists' do
     valid_genre = Genre.find_by(name: genre)
     new_artist << valid_genre
   else
-    "that genre is not in the database"
+    [404,"that genre is not in the database"]
     # this should reroute to a 'create new genre' page
   end
 
