4cf2f48fb4e294c8e1f6540c23c9d33eaec8ff67
Dan Asselin & Malcolm Bouzi & Awesome Ninja Fighter <pair+danasselin.mbouzi.samguergen@devbootcamp.com>
2015-03-21 16:40:57 -0400

    Working create route, persists to database

--- app/controllers/artists.rb
+++ app/controllers/artists.rb
@@ -16,31 +16,49 @@ end
 
 post '/artists' do
 
-  # label = params[:record_label].strip if params[:record_label]
+  label = RecordLabel.find_or_create_by(name: params[:record_label].strip)
   # genre = params[:genre].strip.downcase if params[:genre]
 
-  new_artist = Artist.create(
-                          moniker: params[:moniker],
-                          birth_name:params[:birth_name],
-                          age: params[:age],
-                          description: params[:description],
-                          genre: params[:genre]
+  # new_artist = Artist.create(
+  #                         moniker: params[:moniker],
+  #                         birth_name:params[:birth_name],
+  #                         age: params[:age],
+  #                         description: params[:description],
+  #                         genre: params[:genre]
+  #                         )
+
+
+
+  @new_artist = Artist.new(
+                          :moniker => params[:moniker],
+                          :birth_name => params[:birth_name],
+                          :age => params[:age],
+                          :description => params[:description],
+                          :genres => [Genre.find_or_create_by(:name => params[:genre])],
+                          :record_label_id => label.id
                           )
 
-  if RecordLabel.find_by(name: label)
-    valid_label = Label.find_by(name: label)
-    new_artist.record_label_id = valid_label.id
-  else
-    [500,"that record label is not in the database"]
-    # eventually this should reroute to a 'create new label' page
-  end
+  # if RecordLabel.find_by(name: label)
+  #   valid_label = Label.find_by(name: label)
+  #   new_artist.record_label_id = valid_label.id
+  # else
+  #   [500,"that record label is not in the database"]
+  #   # eventually this should reroute to a 'create new label' page
+  # end
 
-  if Genre.find_by(name: genre)
-    valid_genre = Genre.find_by(name: genre)
-    new_artist << valid_genre
+  # if Genre.find_by(name: genre)
+  #   valid_genre = Genre.find_by(name: genre)
+  #   new_artist << valid_genre
+  # else
+  #   [404,"that genre is not in the database"]
+  #   # this should reroute to a 'create new genre' page
+  # end
+
+
+  if @new_artist.save
+    redirect "/artists/#{@new_artist.id}"
   else
-    [404,"that genre is not in the database"]
-    # this should reroute to a 'create new genre' page
+    [404, "This artist couldn't be created."]
   end
 
 end
