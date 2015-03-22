ff98f30117f984dfc040c6841ecff00ea765b25b
mbouzi <mrbouzi@gmail.com>
2015-03-22 14:53:44 -0400

    completed index. artist/index, edit pages styling

--- app/controllers/albums.rb
+++ app/controllers/albums.rb
@@ -1,2 +1,15 @@
 require 'sinatra'
 
+ get '/artists/:id/albums/new' do
+ 	@artist_to_edit = Artist.find_by(:id => params[:id])
+ 	erb: :'albums/new'
+ end
+
+ get '/artists/:id/albums/edit' do
+ 	@artist_to_edit = Artist.find_by(:id => params[:id])
+ 	erb: :'albums/edit'
+ end
+
+ post '/artist/:id/albums' do
+ 	@new_album = Album.find_or_create_by(:name => params[:album])
+ en
