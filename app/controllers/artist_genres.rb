250a7d36764dcc8d1a9ce67e8b82fe6b77131b46
Dan Asselin <dasselin86@gmail.com>
2015-03-22 14:22:39 -0400

    Working route for adding genres to artists

--- app/controllers/artist_genres.rb
+++ app/controllers/artist_genres.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 
-get '/artist_genres/:id/edit' do
-  @artist_to_edit = Artist.find_by(:id => params[:id])
+get '/artist_genres/:artist_id/edit' do
+  @artist_to_edit = Artist.find_by(:id => params[:artist_id])
   erb :'artist_genres/edit'
 end
+
+put '/artist_genres/:artist_id' do
+  @artists_genres_array = Artist.find_by(:id => params[:artist_id]).genres
+
+  if params[:genre_name] == ""
+    [404, "Sorry, something went wrong."]
+  else
+    @artists_genres_array << Genre.find_or_create_by(:name => params[:genre_name])
+    redirect "/artists/#{params[:artist_id]}"
+  end
+end
+
