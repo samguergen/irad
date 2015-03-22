4f31978ae5834d03b86cb460bf3d98b56840dd15
Samantha Guergenenov <samguergen@gmail.com>
2015-03-21 21:10:10 -0400

    added record labels get route, with record lists and record details page

--- app/controllers/record_label.rb
+++ app/controllers/record_label.rb
@@ -1,3 +1,24 @@
 get '/record_labels' do
-  erb :"record_label/index"
+  @records = RecordLabel.all
+  erb :"record_labels/index"
 end
+
+
+get '/record_labels/:id/edit' do
+  erb :"record_labels/edit"
+end
+
+
+get '/record_labels/:id' do
+  @the_label = RecordLabel.find_by(:id => params[:id])
+  erb :"record_labels/show"
+end
+
+
+get '/record_labels/new' do
+  erb :"record_labels/new"
+end
+
+
+# post '/record_labels' do
+# end
