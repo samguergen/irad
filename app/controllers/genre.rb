81237051adbef97fc73814109bb4991426c0e546
Dan Asselin <dasselin86@gmail.com>
2015-03-22 12:41:21 -0400

    Create new genre route and matching erb

--- app/controllers/genre.rb
+++ app/controllers/genre.rb
@@ -5,8 +5,22 @@ get '/genres' do
   erb :'genres/index'
 end
 
+get '/genres/new' do
+  erb :'genres/new'
+end
+
 get '/genres/:id' do
   @cur_genre = Genre.find_by(:id => params[:id])
   @artist_genres = ArtistGenre.find_by(:genre_id => params[:id])
   erb :'genres/show'
+end
+
+post '/genres' do
+  if Genre.find_by(:name => params[:name])
+    [404, "#{params[:name]} already exists!"]
+  else
+    @new_genre = Genre.create(:name => params[:name])
+    redirect '/genres'
+  end
+
 end
