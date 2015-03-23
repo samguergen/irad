ca3bb73d4356073471eb0ea8e1e7a640c0db019c
Dan Asselin <dasselin86@gmail.com>
2015-03-22 22:18:12 -0400

    Finish edit route for albums

--- app/controllers/albums.rb
+++ app/controllers/albums.rb
@@ -21,13 +21,13 @@ get '/artists/:id/albums/:id/edit' do
 end
 
 put '/artists/:id/albums/:id' do
-
-	@album_to_update = Album.find_by(:id => params[:id])
+  @album_to_update = Album.find_by(:id => params[:id])
+  @the_artist = Artist.find_by(:id => @album_to_update.artist.id)
 	if @album_to_update
 		@album_to_update.title = params[:title]
 		@album_to_update.release_date = params[:release_date]
 		if @album_to_update.save
-			redirect "/artists/#{params[:id]}"
+			redirect "/artists/#{@the_artist.id}/albums"
     	else
       		[500, "Something went wrong!"]
    		end
