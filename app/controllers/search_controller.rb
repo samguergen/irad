5b29d2909691885d725e503ba8f8c4d7948b8814
Dan Asselin <dasselin86@gmail.com>
2015-05-29 13:20:35 -0400

    Add three search queries to search#search action
    In search controller, add search queries to build albums, artists and songs collections

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,10 @@
 class SearchController < ApplicationController
   def search
-
+    if params[:q]
+      term = '%' + params[:q].strip.downcase + '%'
+      @artists = Artist.where('lower(stage_name) like ?', term)
+      @albums = Album.where('lower(title) like ?', term)
+      @songs = Song.where('lower(title) like ?', term)
+    end
   end
 end
