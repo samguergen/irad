55c99b4898f62dc23c93fdfd29e3756c9299d7c3
Dan Asselin <dasselin86@gmail.com>
2015-06-03 12:36:45 -0400

    Auto-update artist table with avatar fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526183303) do
+ActiveRecord::Schema.define(version: 20150603162301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,12 +39,16 @@ ActiveRecord::Schema.define(version: 20150526183303) do
 
   create_table "artists", force: :cascade do |t|
     t.integer  "record_label_id"
-    t.string   "stage_name",      null: false
+    t.string   "stage_name",          null: false
     t.string   "birth_name"
     t.datetime "date_of_birth"
-    t.text     "description",     null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.text     "description",         null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "genres", force: :cascade do |t|
@@ -72,5 +76,14 @@ ActiveRecord::Schema.define(version: 20150526183303) do
 
   add_index "songs", ["album_id"], name: "index_songs_on_album_id", using: :btree
   add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.string   "username",        null: false
+    t.string   "url"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
