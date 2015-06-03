6a07e433faaebdb17fcc6a803ca6dd05f02a5d91
Dan Asselin <dasselin86@gmail.com>
2015-06-02 22:01:17 -0400

    Add presence: true validation to Album model for #title attribute

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,8 +2,11 @@ class Album < ActiveRecord::Base
   belongs_to :artist
   has_many :songs
 
+  validates :title, presence: true
+
   validates :title, uniqueness: {scope: :artist_id, case_sensitive: false,
     message: "Album already exists for this artist"}
+
 
   def to_param
     "#{id}-#{title}"
