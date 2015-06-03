f5cd024213d466d1586756fc5fd486b535975479
Dan Asselin <dasselin86@gmail.com>
2015-06-03 12:30:11 -0400

    Add Paperclip methods to Artist model
    has_attached_file method and validates_attachment_content_type methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,6 +5,12 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :artist_genres
   has_many :songs
 
+  has_attached_file :avatar, styles: {
+    medium: "300x300>",
+    thumb: "100x100>" },
+    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   validates :stage_name, uniqueness: {case_sensitive: false}
   validates :stage_name, :description, presence: true
 
