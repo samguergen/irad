290be9f7c6de768b278f2a0c3395f7f4998f5428
Dan Asselin <dasselin86@gmail.com>
2015-06-06 14:40:31 -0400

    Add RecordLabel#display_founding_date method to RecordLabel model.

--- app/models/record_label.rb
+++ app/models/record_label.rb
@@ -3,4 +3,10 @@ class RecordLabel < ActiveRecord::Base
 
   validates :name, presence: true
   validates :name, uniqueness: {case_sensitive: false}
+
+  def display_founding_date
+    if founding_date != nil
+      "Since #{founding_date}"
+    end
+  end
 end
